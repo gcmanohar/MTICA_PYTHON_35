06632dfe60c69b300eaefa8503913be13dfdc4b3
gcmanohar <112505775+gcmanohar@users.noreply.github.com>
2022-12-26 03:46:33 -0800

    Add files via upload

---
+++ day5_program11_functions11_count_found.py
@@ -0,0 +1,12 @@
+'today is monday and tomorrow is tuesday'.count('o')
+5
+'today is monday and tomorrow is tuesday'.count('day')
+3
+'today is monday and tomorrow is tuesday'.count(' ')
+6
+'today is monday and tomorrow is tuesday'.count(' is')
+2
+'today is monday and tomorrow is tuesday'.find(' day')
+-1
+'today is monday and tomorrow is tuesday'.find(' day')
+-1
